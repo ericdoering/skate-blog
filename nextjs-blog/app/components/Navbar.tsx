61a5a23d71d822808517cf76a5826b3b76763f88
Eric Doering <ericbenjamindoering@gmail.com>
2025-10-02 16:47:30 -0400

    navbar and dark mode added

---
+++ nextjs-blog/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { ModeToggle } from "../ModeToggle";
+
+export default function Navbar() {
+    return (
+        <>
+        <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+            <Link href="/" className="font-bold text-3xl">
+                Insanity<span className="text-primary">Skateboards</span>
+            </Link>
+            <ModeToggle />
+        </nav>
+        </>
+    )
+};
