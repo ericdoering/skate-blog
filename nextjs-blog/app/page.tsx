567c303aead9aa8330e0bd1ac29259163e9674cb
Eric Doering <ericbenjamindoering@gmail.com>
2025-10-04 19:00:59 -0400

    sanity client added and new query created

--- nextjs-blog/app/page.tsx
+++ nextjs-blog/app/page.tsx
@@ -1,4 +1,22 @@
-export default function Home() {
+import { client } from "./lib/sanity"
+
+async function fetchContent() {
+  const query =  `
+  *[_type == 'blog'] | order(createdAt, desc) {
+  title,
+  smallDescription,
+  "currentSlug": slug.current,
+  }`
+
+  const data = await client.fetch(query)
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await fetchContent();
+
+  console.log("DATA =>", data)
   return (
     <>
       <div>
