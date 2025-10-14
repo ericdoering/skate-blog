8c2115add3aebfb5abe97af7f8403735ab831644
Eric Doering <ericbenjamindoering@gmail.com>
2025-10-14 16:57:41 -0400

    additional UI and fields added to blog articles

--- nextjs-blog/app/page.tsx
+++ nextjs-blog/app/page.tsx
@@ -1,7 +1,9 @@
 import { client, urlFor } from "./lib/sanity";
 import { simpleBlogCard } from "../app/lib/interface";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 async function fetchContent() {
   const query =  `
@@ -21,7 +23,7 @@ export default async function Home() {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-5 gap-5">
         {data.map((post, idx) => (
           <Card key={idx}>
             <Image 
@@ -29,7 +31,15 @@ export default async function Home() {
               width={500} 
               src={urlFor(post.titleImage).url()} 
               alt="image" 
+              className="rounded-t-lg h-[200px] object-cover"
             />
+            <CardContent className="mt-5">
+              <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+              <p className="text-sm line-clamp-3 mt-4">{post.smallDescription}</p>
+              <Button className="w-full mt-7" asChild>
+                <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
           </Card>
         ))}
       </div>
