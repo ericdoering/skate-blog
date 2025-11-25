b0b7bdf8411ff465820f2af936d1930079a4e3db
Eric Doering <ericbenjamindoering@gmail.com>
2025-11-25 18:08:16 -0500

    global font change

--- nextjs-blog/app/layout.tsx
+++ nextjs-blog/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "./components/theme-provider"
 import Navbar from "./components/Navbar";
 import "./globals.css";
+import { Tomorrow } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const tomorrow = Tomorrow({
+  variable: "--font-tomorrow",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${tomorrow.variable} antialiased`}>
          <ThemeProvider
             attribute="class"
             defaultTheme="system"
--- nextjs-blog/lib/urlForFile.ts
+++ nextjs-blog/lib/urlForFile.ts
@@ -6,11 +6,11 @@ import type { SanityProjectDetails } from "@sanity/asset-utils";
 export function fileUrl(source: { asset?: { _ref?: string } } | undefined) {
     if (!source?.asset?._ref) return undefined;
   
-    const ref = source.asset._ref; // "file-c21c1b0776b28987f9e52c9fe151f928c00d6427-mov"
+    const ref = source.asset._ref;
     const [_, id, ext] = ref.split("-");
   
-    const projectId = "xidypni3"; // your Sanity projectId
-    const dataset = "production";  // your dataset
+    const projectId = "xidypni3"; 
+    const dataset = "production"; 
   
     return `https://cdn.sanity.io/files/${projectId}/${dataset}/${id}.${ext}`;
   }
